ad03e06bf04ef3e6b876bda6eb7e5522172e3210
Ashar121299 <asharalmomani@yahoo.com>
2022-09-05 16:54:45 +0300

    class11 don

--- src/Header.js
+++ src/Header.js
@@ -6,8 +6,8 @@ class Header extends React.Component {
   render() {
     return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
+        <Navbar.Brand>My Favorite Books ..</Navbar.Brand>
+        <NavItem><Link to="/" className="nav-link">Home page </Link></NavItem>
         {/* PLACEHOLDER: render a navigation link to the about page */}
       </Navbar>
     )
